f40bb2367d5c6fc1bbdb8304bdf632cb7ad589fa
Akira <akira.matsuda@me.com>
2019-07-17 02:47:51 +0900

    Change folder

--- include/parallel_lines.h
+++ include/parallel_lines.h
@@ -1,8 +1,25 @@
-#include "parallel_lines.h"
+#pragma once
+
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <mutex>
+#include <vector>
 
 namespace parallel_lines
 {
-	void scheduler::update() {
+class scheduler
+{
+private:
+	std::mutex mutex;
+	std::vector<std::function<void()>> functions;
+	std::condition_variable cond;
+
+public:
+	scheduler(){};
+	~scheduler(){};
+	void update()
+	{
 		if (!this->functions.empty()) {
 			std::unique_lock<std::mutex> lock(this->mutex);
 			for (int i = 0; i < this->functions.size(); ++i) {
@@ -13,14 +30,16 @@ namespace parallel_lines
 			this->cond.notify_one();
 		}
 	}
-
-	void scheduler::push(std::function<void()> func) {
+	void push(std::function<void()> func)
+	{
 		std::unique_lock<std::mutex> lock(this->mutex);
 		this->functions.push_back(func);
 	}
 
-	parallel_lines::scheduler* scheduler::shared_scheduler() {
+	static parallel_lines::scheduler *shared_scheduler()
+	{
 		static parallel_lines::scheduler shared_instance;
 		return &shared_instance;
 	}
+};
 }
